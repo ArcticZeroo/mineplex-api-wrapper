1895e2a7b26b80a76aee2203c37a45ed02a82d95
Spencer <spencer@artix.cc>
2016-11-17 21:58:24 -0500

    add toHumanReadable prototype and rename stats to playerStats

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var Prototypes         = require('./lib/Prototypes');
+require('./lib/Prototypes');
 var MineplexAPIWrapper = require(`./lib/MineplexAPIWrapper`);
 module.exports         = MineplexAPIWrapper;
